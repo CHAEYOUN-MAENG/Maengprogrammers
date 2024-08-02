b556066778c2783d88cf5dd93c72f773e880aa3b
CHAEYOUN-MAENG <koung3132@naver.com>
2024-08-02 20:21:19 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        int a = num1;
+        int b = num2;
+        
+        answer = a - b;
+        
+        return answer;
+    }
+}
