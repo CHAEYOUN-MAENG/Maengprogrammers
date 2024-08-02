2760a28273478ad1a5722630b5556e2b6e0e88a1
CHAEYOUN-MAENG <koung3132@naver.com>
2024-08-02 20:12:25 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        
+        int a = 2022;
+        answer = a - age + 1; 
+        
+        return answer;
+    }
+}
